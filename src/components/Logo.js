245734da7ddc852ea126d0a6b0a67ddf4ddbd619
Khozenn <59557249+Khozenn@users.noreply.github.com>
2022-05-11 11:54:11 +0200

    Add files via upload

---
+++ src/components/Logo.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Logo = () => {
+  return (
+    <div className="logo">
+      <img alt="logo" src="./logo192.png" />
+      <h3>Hello World!</h3>
+    </div>
+  );
+};
+
+export default Logo;
---
+++ src/pages/About.js
@@ -0,0 +1,63 @@
+import React from "react";
+import Logo from "../components/Logo";
+import Navigation from "../components/Navigation";
+
+const About = () => {
+  return (
+    <div>
+      <Logo />
+      <Navigation />
+      <h1>About</h1>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eum aliquid,
+        facere consequatur sunt libero illo, autem vel voluptas ex unde cum
+        maiores, quisquam earum voluptatem possimus in modi odit accusamus
+        fugiat quidem voluptate veniam hic. Ipsam dignissimos cumque impedit.
+        Officiis consequuntur culpa ipsam dolorum quos vel doloremque cumque et,
+        nam, dolores rerum laudantium in. Vero delectus omnis, modi natus magnam
+        debitis quaerat velit dolore voluptatum atque quidem quas. Ad,
+        repellendus recusandae blanditiis voluptatem, facere modi doloribus
+        tenetur officia porro repellat ea dolorem! Amet nemo architecto, nam
+        deleniti nobis voluptatum et expedita in blanditiis dolorem magni,
+        quidem hic? Odio reiciendis nam recusandae ut eum, nisi, dolorum ea sunt
+        soluta at unde nobis corporis ipsa natus quae officia delectus dolorem
+        neque labore architecto sequi illum ex voluptates. Quo id eos non
+        veritatis laudantium minus temporibus eveniet impedit molestiae! Vel, at
+        aliquid optio ex inventore fugiat autem fuga. Nostrum veniam sequi
+        repellat quam nihil porro deleniti voluptatum placeat fugit voluptas
+        quis alias, repellendus at expedita rem blanditiis, labore, sit dolore.
+        Dicta, praesentium aut! Minima ipsum distinctio odio laboriosam tempora,
+        quisquam modi quasi veniam soluta inventore cumque, velit eligendi
+        fugiat pariatur dolorum! Reprehenderit quos possimus voluptates
+        excepturi maxime, unde voluptas voluptatem voluptatum temporibus nam.
+      </p>
+      <br />
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aut amet neque
+        nisi tempora aspernatur, distinctio, magni eos porro enim maxime
+        incidunt harum placeat voluptatum quas. Fugiat numquam quidem nihil
+        provident ullam accusantium enim eaque veritatis perspiciatis ab illum
+        qui tempora, minima voluptatibus perferendis ea! Unde, pariatur? Fugit
+        nisi debitis error, laudantium explicabo expedita in obcaecati et veniam
+        eaque quod quidem, possimus perspiciatis enim molestias est maxime
+        repellat ex necessitatibus soluta reprehenderit. Enim natus itaque
+        similique quibusdam magni fuga vero beatae dolorem, quam maiores? Ullam
+        dolores consectetur cupiditate molestias aliquid tempore libero dolorum
+        repellat obcaecati quo fugit neque incidunt esse veniam quas, enim eos,
+        ex culpa laboriosam provident amet, quia vel? Ad corporis nemo quas
+        velit aliquid asperiores eos. Reiciendis alias necessitatibus minus est,
+        delectus cum sequi earum sunt nisi quae exercitationem obcaecati quis,
+        laboriosam in natus officia blanditiis, veritatis sed nobis quod placeat
+        neque itaque aperiam! Praesentium maxime corrupti laboriosam sint
+        consectetur eveniet itaque obcaecati alias! Aliquam, animi recusandae
+        accusamus sequi quia libero distinctio, est consectetur alias sapiente
+        quaerat asperiores quas harum fuga omnis, optio repellendus neque
+        dignissimos iusto officiis adipisci corporis eaque! Repudiandae beatae
+        libero nam atque harum ab quos praesentium sequi veritatis, nobis non
+        dolore alias? Possimus, rerum.
+      </p>
+    </div>
+  );
+};
+
+export default About;
